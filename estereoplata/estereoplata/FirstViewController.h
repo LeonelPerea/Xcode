bc409b46a441e974ff56831699b3c186535a9d9d
Leonel Perea <iticleonel.len@gmail.com>
2013-09-13 22:17:40 -0500

    Estereo Plata - Player working :D

---
+++ estereoplata/estereoplata/FirstViewController.h
@@ -0,0 +1,17 @@
+//
+//  FirstViewController.h
+//  estereoplata
+//
+//  Created by Leonel Roberto Perea Trejo on 12/09/13.
+//  Copyright (c) 2013 Leonel Roberto Perea Trejo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AudioStreamer;
+
+@interface FirstViewController : UIViewController{
+    AudioStreamer *streamer;
+}
+
+@end
---
+++ estereoplata/estereoplata/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  estereoplata
+//
+//  Created by Leonel Roberto Perea Trejo on 12/09/13.
+//  Copyright (c) 2013 Leonel Roberto Perea Trejo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
